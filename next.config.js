33f593d385729fd4ab93843a552a7217f5750175
luiztauffer <24541631+luiztauffer@users.noreply.github.com>
2026-05-06 13:45:32 +0200

    use domain

--- next.config.js
+++ next.config.js
@@ -2,11 +2,6 @@ const withImages = require('next-images');
 
 module.exports = withImages({
   output: 'export',
-  basePath: '/portfolio',
-  assetPrefix: '/portfolio/',
-  env: {
-    NEXT_PUBLIC_BASE_PATH: '/portfolio',
-  },
   compiler: {
     styledComponents: true
   },
